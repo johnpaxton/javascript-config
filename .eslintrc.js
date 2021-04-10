3788aed40f9a0ef7d38adb42410c5eb9362692ee
John Paxton <pax@speedingplanet.com>
2021-04-09 20:30:24 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: [ '@babel' ],
+  parser: '@babel/eslint-parser',
+
+  extends: [ 'standard', 'standard-with-typescript' ],
+  rules: {
+    semi: [ 'error', 'always' ],
+    'max-len': [ 'error', { code: 90, comments: 120 } ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'space-before-function-paren': [ 'error', 'never' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'newline-per-chained-call': [ 'error', { ignoreChainWithDepth: 2 } ],
+    indent: [ 'error', 2, { MemberExpression: 1 } ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'space-in-parens': [ 'error', 'always' ],
+    'object-property-newline': [
+      'error',
+      { allowAllPropertiesOnSameLine: true },
+    ],
+    'prefer-const': 'off',
+  },
+  ignorePatterns: [ 'node_modules' ],
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript Config
+
+I wanted a repo that collects my various JavaScript config files. This repo should **not** be extended/forked/whatever as a baseline. Since I work in several different environments, and with several different tools (especially in the code formatting and testing areas), I need a clearinghouse to grab the appropriate configs for whatever project I'm working on.
+
+At the moment, it's only one set of files. That could change later.
