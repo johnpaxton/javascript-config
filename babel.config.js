12e4d18bedb3fadb43820293091991e79ff3517d
joh05778_tjxinc <john_paxton@tjx.com>
2023-02-26 16:04:41 -0500

    Reformat

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-module.exports = function( api ) {
-  api.cache( true );
+module.exports = function (api) {
+  api.cache(true);
 
-  const presets = [ '@babel/env' ];
+  const presets = ['@babel/env'];
 
   return {
     presets,
